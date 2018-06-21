fadb892375356b84ef2c656755da3eaf177cacf1
Miquel A. Cabot <miquel.cabot@gmail.com>
2018-06-21 23:05:02 +0200

    Set contract name as a constant value

--- compile.js
+++ compile.js
@@ -1,8 +1,11 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
 
-const contractPath = path.resolve(__dirname, 'contracts', 'ExampleContract.sol');
-const source = fs.readFileSync(contractPath, 'utf8');
+const CONTRACT_NAME = "ExampleContract";
 
-module.exports = solc.compile(source, 1).contracts[':ExampleContract'];
+var contractPath = path.resolve(__dirname, "contracts", CONTRACT_NAME+".sol");
+var contractSource = fs.readFileSync(contractPath, "utf8");
+
+// solc.compile generates a JSON output
+module.exports = solc.compile(contractSource, 1).contracts[":"+CONTRACT_NAME];
